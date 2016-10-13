23db88b71079f6ec273c0dff7fe14fa08b74d270
Timbo Cole <timbo_cole@yahoo.co.uk>
2016-10-14 00:49:06 +0100

    All commenting is done to date, Logo/favicon created/implemented

--- assets/header.php
+++ assets/header.php
@@ -1,20 +1,22 @@
 <!-- Header -->
-	<!-- NAVIGATION BAR -->
+<!-- NAVIGATION BAR -->
 
-		<nav class="navbar navbar-default">
-		  <div class="container-fluid">
-		    <div class="navbar-header">
-		      <a class="navbar-brand" href="./"><img src="./img/title-logo-trans-small.png" alt="Tetris Logo"></a>
-		    </div><!-- /.navbar-header -->
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="./"><img src="./img/logo-small-trans.png" alt="Tetris Logo"></a>
+        </div><!-- /.navbar-header -->
 
-		    <!-- Collect the nav links, forms, and other content for toggling -->
-		    <div class="collapse navbar-collapse" id="main-navbar">
-		      <ul class="nav navbar-nav navbar-right">
-		        <li <?php if ((stripos($_SERVER['REQUEST_URI'],'index.php') !== false) OR (substr($_SERVER['REQUEST_URI'], -1) == "/")) {echo 'class="active"';} ?> ><a href="./">Play<?php if (stripos($_SERVER['REQUEST_URI'],'index.php') !== false) {echo '<span class="sr-only">(current)</span>';} ?></a></li>
+        <!-- Navigation Buttons -->
+        <!-- Read in the URL of the requested page and set the corresponding page button to the 'active' state -->
+        
+        <div class="collapse navbar-collapse" id="main-navbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li <?php if ((stripos($_SERVER['REQUEST_URI'],'index.php') !== false) OR (substr($_SERVER['REQUEST_URI'], -1) == "/")) {echo 'class="active"';} ?> ><a href="./">Play<?php if (stripos($_SERVER['REQUEST_URI'],'index.php') !== false) {echo '<span class="sr-only">(current)</span>';} ?></a></li>
 
-				<li <?php if (stripos($_SERVER['REQUEST_URI'],'book.php') !== false) {echo 'class="active"';} ?> ><a href="./book.php">Scores<?php if (stripos($_SERVER['REQUEST_URI'],'book.php') !== false) {echo '<span class="sr-only">(current)</span>';} ?></a></li>
-		      </ul>
-		     
-		    </div><!-- /.navbar-collapse -->
-		  </div><!-- /.container-fluid -->
-		</nav>
+                <li <?php if (stripos($_SERVER['REQUEST_URI'],'book.php') !== false) {echo 'class="active"';} ?> ><a href="./book.php">Scores<?php if (stripos($_SERVER['REQUEST_URI'],'book.php') !== false) {echo '<span class="sr-only">(current)</span>';} ?></a></li>
+            </ul>
+
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <!-- Declare Website Title (Shown in title bar of web browser) -->
         <title>Tetris</title>
         <!-- Declare Favicon -->
-        <link rel="icon" href="./favicon.ico" type="image/x-icon">
+        <link rel="icon" href="./img/favicon.png" type="image/x-icon">
         <!-- Create link to Bootstrap CSS file (THE CODE IN THIS FILE IS NOT MY CODE!) -->
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
         <!-- Create link to my own CSS file (used to overwrite any CSS per my preference) -->
@@ -18,7 +18,6 @@
     <body>
         <!-- Display the navbar/header from the base file -->
         <?php require'assets/header.php'; ?>
-        
         
         <!-- Display the Footer from the base file -->
         <?php require 'assets/footer.php';?>
