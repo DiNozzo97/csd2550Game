6ce2842af19cad4e1aea3917db127a1924fe0bcc
Timbo Cole <timbo_cole@yahoo.co.uk>
2016-10-13 17:51:56 +0100

    Added most of the initial comments
    Still need to add comments to Header.php

--- assets/footer.php
+++ assets/footer.php
@@ -1,9 +1,8 @@
 <!-- Footer -->
 <div class='navbar navbar-default navbar-fixed-bottom'>
-		<div class='container'>
-			<p id='built-by' class='navbar-text'>&copy; Timothy Cole <?php echo date("Y"); ?></p>
-		</div>
-	</div>
-
-		<script src='http://code.jquery.com/jquery-1.11.3.min.js'></script>
-		<script type='text/javascript' src='js/bootstrap.js'></script>
+    <!-- Container div, used to add nice spacing -->
+    <div class='container'>
+        <!-- Text to display in footer (Fetches current year from server time so copyright information is always up to date) -->
+        <p id='built-by' class='navbar-text'>&copy; Timothy Cole <?php echo date("Y"); ?> All Rights Reserved</p>
+    </div><!-- /.container -->
+</div><!-- /.navbar -->
--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
+<!---->
 <html>
-<head>
-	<title>Tetris</title>
+    <head>
+        <!-- Declare Website Title (Shown in title bar of web browser) -->
+        <title>Tetris</title>
+        <!-- Declare Favicon -->
+        <link rel="icon" href="./favicon.ico" type="image/x-icon">
+        <!-- Create link to Bootstrap CSS file (THE CODE IN THIS FILE IS NOT MY CODE!) -->
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <!-- Create link to my own CSS file (used to overwrite any CSS per my preference) -->
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <!-- Import the JQuery Library (THE CODE IN THIS FILE IS NOT MY CODE!) -->
+        <script src='js/jquery-3.1.1.min.js'></script>
+        <!-- Import the Bootstrap JS Library (THE CODE IN THIS FILE IS NOT MY CODE!) -->
+        <script src='js/bootstrap.min.js'></script> 
+    </head>
+    <body>
+        <!-- Display the navbar/header from the base file -->
+        <?php require'assets/header.php'; ?>
+        
+        
+        <!-- Display the Footer from the base file -->
+        <?php require 'assets/footer.php';?>
 
-	<?php require'assets/header.php'; ?>
-
-	<?php require 'assets/footer.php';?>
-
-</body>
+    </body>
 </html>
